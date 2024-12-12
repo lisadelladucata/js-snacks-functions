7817f44330d7d9dbf651434848fc794807142ac8
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-12 16:49:37 +0100

    feat:add-first-exercise

---
+++ 01-iniziali/script.js
@@ -0,0 +1,22 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+// Dichiara la funzione qui.
+
+function firstLetter (names){
+    const letter=[];
+    for (let i= 0; i < names.length; i++){
+        letter.push(names[i][0])
+    }
+    return letter;
+    
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+const letter= firstLetter(names)
+console.log(letter)
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
